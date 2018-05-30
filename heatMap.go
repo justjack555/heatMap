49d86c209d38f777be6d6af2c9e5ec82df540bf0
Jack <jjricci555@gmail.com>
2018-05-30 13:36:21 -0400

    [Twitter] Debug 403 forbidden on attempt to obtain bearer token

--- heatMap.go
+++ heatMap.go
@@ -3,24 +3,54 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/justjack555/heatMap/media"
 )
 
+func invokeTwitter() error {
+	var twitterCred strings.Builder
+//	twitterKey := os.Args[1]
+//	twitterSec := os.Args[2]
+	query := os.Args[3]
+
+	// Concatenate key, colon, and secret
+	for i, str := range os.Args[1:3] {
+		fmt.Println("INVOKE_TWITTER: Index value ", i, ", arg value: ", str)
+		twitterCred.WriteString(str)
+
+		// Append colon after consumer key
+		if i == 0 {
+			twitterCred.WriteString(":")
+		}
+	}
+
+	fmt.Println("INVOKE_TWITTER: Final twitter credential: ", twitterCred.String())
+
+	// With full credential, send to GetTweets
+	twitterRes, err := media.GetTweets(twitterCred.String(), query)
+	if err != nil {
+		fmt.Println("Error message is: ", err)
+		return err
+	}
+
+	fmt.Println("Result of Get_Tweets is: ", twitterRes)
+
+	return nil
+}
+
 func main(){
-	if len(os.Args[1:]) != 2 {
-		fmt.Println("Usage: go run heatMap.go <TWITTER_API_KEY> <QUERY>")
+	if len(os.Args[1:]) != 3 {
+		fmt.Println("Usage: go run heatMap.go <TWITTER_API_KEY> <TWITTER_SECRET_KEY> <QUERY>")
 		return
 	}
 
-	twitterKey := os.Args[1]
-	query := os.Args[2]
+	// Invoke twitter handler to retrieve tweets
+	err := invokeTwitter()
 
-	twitterRes, err := media.GetTweets(twitterKey, query)
 	if err != nil {
 		fmt.Println("Error message is: ", err)
 		return
 	}
 
-	fmt.Println("Result of Get_Tweets is: ", twitterRes)
 }
--- media/twitter.go
+++ media/twitter.go
@@ -3,22 +3,91 @@ package media
 import (
 	"fmt"
 	"strings"
-	"encoding/json"
+	"encoding/base64"
+	"net/url"
+	"net/http"
+//	"encoding/json"
 )
 
 const resourceUrl string = "https://api.twitter.com/1.1/search/tweets.json"
 const startQuery string = "?q="
-const locationPin string = "39.8283,-98.5795,1500mi"
+const locationPin string = "&geocode=39.8283,-98.5795,1500mi"
+
+const contentType string = `application/x-www-form-urlencoded;charset=UTF-8`
+const tokenEndpoint string = "https://api.twitter.com/oauth2/token"
+var bearerToken string = ""
+
+/**
+ Function to obtain Bearer Token for GET request
+
+ Makes POST request to twitter App Auth API
+ */
+func postCred (apiKey string)(string, error){
+	var authString strings.Builder
+
+	// Make custom client
+	client := &http.Client{}
+
+	// Add body
+	body := url.Values{}
+	body.Set("grant_type", "client_credentials")
+
+	// Build custom request object
+	req, err := http.NewRequest("POST", tokenEndpoint, strings.NewReader(body.Encode()))
+	if err != nil {
+		fmt.Println("Post_CRED: Error creating custom request. Exiting.")
+		return "", err
+	}
+
+	// Bearer + apiKey
+	keyEnc := base64.StdEncoding.EncodeToString([]byte(apiKey))
+	n, strErr := authString.WriteString("Bearer ")
+	if strErr != nil  || n != len("Bearer ") {
+		fmt.Println("Post_CRED: Error writing Authorization to buffer. Exiting.")
+		return "", strErr
+	}
+
+	n, strErr = authString.WriteString(keyEnc)
+	if strErr != nil  || n != len(keyEnc) {
+		fmt.Println("Post_CRED: Error writing Authorization to buffer. Exiting.")
+		return "", strErr
+	}
+
+	// Add two custom headers
+	req.Header.Add("Content-Type", contentType)
+	req.Header.Add("Authorization", authString.String())
+
+	// Make POST request
+	resp, postErr := client.Do(req)
+	if postErr != nil {
+		fmt.Println("Post_CRED: Error with POST to Auth. Exiting.")
+		return "", postErr
+	}
+
+	fmt.Println("POST_CRED: Status code of post is: ", resp.Status)
+
+	return "", nil
+}
 
 func GetTweets(apiKey string, query string) (string, error) {
-	fmt.Println("GET_TWEETS: Returning tweets...")
+	fmt.Println("GET_TWEETS: Returning tweets using API KEY ", apiKey, "...")
 	var fullQuery strings.Builder
+	var authErr error
+
+	// First Authenticate Application
+	bearerToken, authErr = postCred(apiKey)
+	if authErr != nil {
+		fmt.Println("GET_TWEETS: Error Receiving bearer token. Exiting")
+		return "", authErr
+	}
+	// Add base url
 	n, err := fullQuery.WriteString(resourceUrl)
 	if err != nil  || n != len(resourceUrl) {
 		fmt.Println("GET_TWEETS: Error writing query to buffer. Exiting.")
 		return "", err
 	}
 
+	// Query indicator
 	n, err = fullQuery.WriteString(startQuery)
 	if err != nil  || n != len(startQuery) {
 		fmt.Println("GET_TWEETS: Error writing query to buffer. Exiting.")
@@ -33,7 +102,14 @@ func GetTweets(apiKey string, query string) (string, error) {
 		return "", err
 	}
 
-	reqMap := map[string]string{
+	// Add location to request
+	n, err = fullQuery.WriteString(locationPin)
+	if err != nil  || n != len(locationPin) {
+		fmt.Println("GET_TWEETS: Error writing query to buffer. Exiting.")
+		return "", err
+	}
+
+/*	reqMap := map[string]string{
 		"q" : fullQuery.String(),
 		"geocode" : locationPin,
 	}
@@ -43,6 +119,6 @@ func GetTweets(apiKey string, query string) (string, error) {
 		fmt.Println("GET_TWEETS: Error encoding map in JSON. Exiting.")
 		return "", err
 	}
-
-	return string(reqJson), nil
+*/
+	return fullQuery.String(), nil
 }
